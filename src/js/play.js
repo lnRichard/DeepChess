28f37edd68ddde8c4fc4b26c92037a58cc10e273
lnRichard <2004040203@sondervick.nl>
2022-02-12 11:00:31 +0100

    Add type 2 eval

--- src/js/play.js
+++ src/js/play.js
@@ -1,9 +1,10 @@
 "use strict";
 /* eslint-disable no-undef */
 const evaluation_field = document.getElementById("evaluation");
-const elo_mod_field = document.getElementById("elo_mod");
+const elo_mod_field = document.getElementById("elo-mod");
 const turn_history_field = document.getElementById("history");
-const turn_history_container = document.getElementById("history_container");
+const turn_history_container = document.getElementById("history-container");
+const type_2_eval = document.getElementById("type-2-eval");
 
 // Augment DOM
 Element.prototype.remove = function () {
@@ -98,6 +99,30 @@ var last_player_move = null;
 var latest_turn = null;
 var turn_history = [];
 var turn_id = 0;
+var type_2_eval_active = false;
+var type_2_eval_line = 8;
+var type_2_eval_skip = true;
+var type_2_eval_board = {};
+reset_type_2_eval_board();
+
+// Reset type 2 eval board
+function reset_type_2_eval_board() {
+	type_2_eval_board = {
+		"a1": 0, "a2": 0, "a3": 0, "a4": 0, "a5": 0, "a6": 0, "a7": 0, "a8": 0,
+		"b1": 0, "b2": 0, "b3": 0, "b4": 0, "b5": 0, "b6": 0, "b7": 0, "b8": 0,
+		"c1": 0, "c2": 0, "c3": 0, "c4": 0, "c5": 0, "c6": 0, "c7": 0, "c8": 0,
+		"d1": 0, "d2": 0, "d3": 0, "d4": 0, "d5": 0, "d6": 0, "d7": 0, "d8": 0,
+		"e1": 0, "e2": 0, "e3": 0, "e4": 0, "e5": 0, "e6": 0, "e7": 0, "e8": 0,
+		"f1": 0, "f2": 0, "f3": 0, "f4": 0, "f5": 0, "f6": 0, "f7": 0, "f8": 0,
+		"g1": 0, "g2": 0, "g3": 0, "g4": 0, "g5": 0, "g6": 0, "g7": 0, "g8": 0,
+		"h1": 0, "h2": 0, "h3": 0, "h4": 0, "h5": 0, "h6": 0, "h7": 0, "h8": 0,
+	};
+}
+
+// Convert line to letter
+function line_to_letter(line) {
+	return ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'][line]
+}
 
 // Updates stockfish's parameters
 function update_stockfish() {
@@ -247,15 +272,20 @@ function turn_back_turn(turn) {
 		let turn = turn_history.pop();
 		console.log("[!] Revering move: " + i);
 
+		// Reset board
+		type_2_eval_board = turn["PLAYER"]["EVAL_BOARD"];
+
 		// Revert AI move
 		if (!turn["AI"]["CASTLING"]) {
 			console.log("[*] {AI} FROM: ", turn["AI"]["FROM"], turn["AI"]["FROM_PIECE"], " TO: ", turn["AI"]["TO"], turn["AI"]["TO_PIECE"]);
 			if (turn["AI"]["TO_PIECE"]) {
+				game.remove(turn["AI"]["TO"]);
 				game.put(turn["AI"]["TO_PIECE"], turn["AI"]["TO"]);
 			} else {
 				game.remove(turn["AI"]["TO"]);
 			}
 
+			game.remove(turn["AI"]["FROM_PIECE"]);
 			game.put(turn["AI"]["FROM_PIECE"], turn["AI"]["FROM"]);
 		} else if (turn["AI"]["CASTLING"]) {
 			if (turn["AI"]["CASTLING"] == "O-O") {
@@ -277,11 +307,13 @@ function turn_back_turn(turn) {
 		if (!turn["PLAYER"]["CASTLING"]) {
 			console.log("[*] {PLAYER} FROM: ", turn["PLAYER"]["FROM"], turn["PLAYER"]["FROM_PIECE"], " TO: ", turn["PLAYER"]["TO"], turn["PLAYER"]["TO_PIECE"]);
 			if (turn["PLAYER"]["TO_PIECE"]) {
+				game.remove(turn["PLAYER"]["TO"]);
 				game.put(turn["PLAYER"]["TO_PIECE"], turn["PLAYER"]["TO"]);
 			} else {
 				game.remove(turn["PLAYER"]["TO"]);
 			}
 
+			game.remove(turn["PLAYER"]["FROM_PIECE"]);
 			game.put(turn["PLAYER"]["FROM_PIECE"], turn["PLAYER"]["FROM"]);
 		} else if (turn["PLAYER"]["CASTLING"]) {
 			if (turn["PLAYER"]["CASTLING"] == "O-O") {
@@ -425,54 +457,65 @@ stockfish.onmessage = function (event) {
 				}
 			}, 500);
 		} else if (ai_hint) {
-			// Check for game over
-			var possibleMoves = game.moves()
-			if (possibleMoves.length === 0) return
+			try {
+				// Check for game over
+				var possibleMoves = game.moves()
+				if (possibleMoves.length === 0) return
 
-			// Fetch move and type of the piece
-			const move = event.data.split(" ")[1];
-			const from = move.substring(0, 2);
-			const to = move.substring(2, 4);
-			highlightSquareHint(from)
-			highlightSquareHint(to)
-			console.log("[!] AI HINT: " + from + " -> " + to);
-			update_stockfish()
+				// Fetch move and type of the piece
+				const move = event.data.split(" ")[1];
+				const from = move.substring(0, 2);
+				const to = move.substring(2, 4);
+				highlightSquareHint(from)
+				highlightSquareHint(to)
+				console.log("[!] AI HINT: " + from + " -> " + to);
+				update_stockfish()
 
-			// Update state
-			ai_hint = false;
+				// Update state
+				ai_hint = false;
+			} catch(ex) {
+				console.log("[!] AI HINT ERROR: " + ex)
+				update_stockfish()
+			}
 		}
 
 	// AI EVALUATION
 	} else if (event.data.includes("Final evaluation")) {
-		let evaluation = parseFloat(event.data.replaceAll("Final evaluation       ", "").replaceAll(" (white side)", ""));
-		let elo_mod = 0
+		try {
+			let evaluation = parseFloat(event.data.replaceAll("Final evaluation       ", "").replaceAll(" (white side)", ""));
+			let elo_mod = 0
 
-		// Process evaluation
-		if (eval_history.length > 0) {
-			if (!isNaN(evaluation)) {
-				let turn_evaluation = (evaluation - eval_history[eval_history.length - 1]);
-				console.log("[*] Turn Evaluation: " + turn_evaluation + " {white}");
-				elo_mod = turn_evaluation
+			// Process evaluation
+			if (eval_history.length > 0) {
+				if (!isNaN(evaluation)) {
+					let turn_evaluation = (evaluation - eval_history[eval_history.length - 1]);
+					console.log("[*] Turn Evaluation: " + turn_evaluation + " {white}");
+					elo_mod = turn_evaluation
+				} else {
+					console.log("[*] Turn Evaluation: 0 {white}");
+					evaluation = eval_history[eval_history.length - 1];
+					elo_mod = 0;
+				}
 			} else {
-				console.log("[*] Turn Evaluation: 0 {white}");
-				evaluation = eval_history[eval_history.length - 1];
-				elo_mod = 0;
+				elo_mod = evaluation
 			}
-		} else {
-			elo_mod = evaluation
+
+			// Add elo to player
+			console.log("[*] Evaluation: " + evaluation + " {white}");
+			evaluation_field.innerHTML = (Math.round(evaluation * 100) / 100);
+			modify_field_highlight(evaluation_field, evaluation);
+			elo_mod_field.innerHTML = (elo_mod >= 0 ? "+" : "") + (Math.round(elo_mod * 100) / 100);
+			modify_field_highlight(elo_mod_field, elo_mod);
+			add_elo(elo_mod)
+			eval_history.push(evaluation);
+
+			// Update turn
+			update_turn_history(latest_turn);
+		} catch (ex) {
+			console.log("[!] AI EVAL ERROR: " + ex);
+			eval_history.push(eval_history[eval_history.length - 1] ? eval_history[eval_history.length - 1] : 0);
+			update_turn_history(latest_turn);
 		}
-
-		// Add elo to player
-		console.log("[*] Evaluation: " + evaluation + " {white}");
-		evaluation_field.innerHTML = (Math.round(evaluation * 100) / 100);
-		modify_field_highlight(evaluation_field, evaluation);
-		elo_mod_field.innerHTML = (elo_mod >= 0 ? "+" : "") + (Math.round(elo_mod * 100) / 100);
-		modify_field_highlight(elo_mod_field, elo_mod);
-		add_elo(elo_mod)
-		eval_history.push(evaluation);
-
-		// Update turn
-		update_turn_history(latest_turn);
 	} else if (event.data.includes("Pawns")) {
 		let [w_mg, w_eg, b_mg, b_eg] = parse_type1_eval(event.data, "Pawn");
 		update_numeric_field("mg-pawn-strength", (w_mg - b_mg));
@@ -513,6 +556,34 @@ stockfish.onmessage = function (event) {
 		let [w_mg, w_eg, b_mg, b_eg] = parse_type1_eval(event.data, "Space")
 		update_numeric_field("mg-space", (w_mg - b_mg));
 		update_numeric_field("eg-space", (w_eg - b_eg));
+	} else if (event.data.includes("NNUE derived piece values")) {
+		type_2_eval_active = true;
+	} else if (type_2_eval_active && event.data.includes("|")) {
+		if (type_2_eval_skip === true) {
+			// Skip current line
+			type_2_eval_skip = false;
+		} else {
+			// Update parsed line
+			type_2_eval_skip = true;
+			console.log(`[*] TYPE 2 EVAL ${type_2_eval_line }:`, event.data)
+			let type_2_eval_array = event.data.split("|");
+			for (let i = 1; i < (type_2_eval_array.length - 1); i++) {
+				const element = type_2_eval_array[i];
+				type_2_eval_board[line_to_letter((i - 1)) + type_2_eval_line] = parseFloat(element) ? parseFloat(element) : 0;
+			}
+
+			type_2_eval_line -= 1;
+			// Check end
+			if (type_2_eval_line <= 0) {
+				type_2_eval_line = 8;
+				type_2_eval_skip = true;
+				type_2_eval_active = false;
+				console.log("[*] TYPE 2 EVAL BOARD: ", type_2_eval_board);
+			}
+		}
+
+		// Clear div
+		type_2_eval.innerHTML = "";
 	}
 }
 
@@ -617,7 +688,7 @@ function onDragStart(source, piece) {
 // Drop piece
 let firstMove = true;
 function onDrop(source, target) {
-	removeHighlightSquares()
+	removeHighlightSquares();
 
 	// Set last player move
 	let castling = null;
@@ -632,7 +703,17 @@ function onDrop(source, target) {
 	}
 
 	// Set last move
-	last_player_move = { "FROM": source, "TO": target, "FROM_PIECE": piece_from(source), "TO_PIECE": piece_from(target), "PROMOTION": promotion, "CASTLING": castling};
+	last_player_move = { 
+		"FROM": source, 
+		"TO": target, 
+		"FROM_PIECE": piece_from(source), 
+		"TO_PIECE": piece_from(target), 
+		"PROMOTION": promotion, 
+		"CASTLING": castling,
+		"EVAL_BOARD": type_2_eval_board,
+	};
+
+	reset_type_2_eval_board();
 
 	// see if the move is legal
 	let move = game.move({
@@ -659,6 +740,15 @@ function onDrop(source, target) {
 
 // Mouse over square
 function onMouseoverSquare(square, piece) {
+	let type_2 = type_2_eval_board[square]
+	if (type_2 > 0) {
+		type_2_eval.innerHTML = `${square}:&nbsp;<span class="positive">${type_2}</span>`;
+	} else if (type_2 < 0) {
+		type_2_eval.innerHTML = `${square}:&nbsp;<span class="negative">${type_2}</span>`;
+	} else {
+		type_2_eval.innerHTML = `${square}:&nbsp;<span class="neutral">${type_2}</span>`;
+	}
+
 	// get list of possible moves for this square
 	let moves = game.moves({
 		square: square,
@@ -705,7 +795,7 @@ function makeAIMove() {
 	// Generate move
 	setTimeout(() => {
 		// TODO: Use movetime along side depth
-		stockfish.postMessage(`go depth ${get_depth()}` /*+ ` movetime -1`*/);
+		stockfish.postMessage(`go depth ${get_depth()}` + ` movetime -1`);
 	}, 1000);
 }
 
@@ -743,6 +833,7 @@ var config = {
 // Start the game by creating the board
 board = Chessboard('chessboard', config)
 
+// ! BUG: Only the pawn type 1 evaluation gets updated for some reason at some cases at some times
 // TODO: Fix rookering en en passant turn back time to the good old days
 // TODO: Add move time mechanic
 // TODO: Balance AI difficulty
@@ -766,27 +857,3 @@ board = Chessboard('chessboard', config)
 // TODO: Show moves towards check by M1, M2, etc.
 // *TODO: Find move variations, and show them on the board
 // TODO: Type 2 evaluation system
-
-/* Default values at the beginning of the game
-[*] White Pawn Evaluation: {MG: 0.38, EG: -0.08}
-[*] Black Pawn Evaluation: {MG: 0.38, EG: -0.08}
-[*] White Knight Evaluation: {MG: -0.02, EG: -0.19}
-[*] Black Knight Evaluation: {MG: -0.02, EG: -0.19}
-[*] White Bishop Evaluation: {MG: 0.01, EG: -0.41}
-[*] Black Bishop Evaluation: {MG: 0.01, EG: -0.41}
-[*] White Rook Evaluation: {MG: -0.26, EG: -0.06}
-[*] Black Rook Evaluation: {MG: -0.26, EG: -0.06}
-[*] White Queen Evaluation: {MG: 0.00, EG: 0.00}
-[*] Black Queen Evaluation: {MG: 0.00, EG: 0.00}
-[*] White Mobility Evaluation: {MG: -0.88, EG: -1.15}
-[*] Black Mobility Evaluation: {MG: -0.88, EG: -1.15}
-[*] White King safety Evaluation: {MG: 0.91, EG: -0.10}
-[*] Black King safety Evaluation: {MG: 0.91, EG: -0.10}
-[*] White Threat Evaluation: {MG: 0.00, EG: 0.00}
-[*] Black Threat Evaluation: {MG: 0.00, EG: 0.00}
-[*] White Passed Evaluation: {MG: 0.00, EG: 0.00}
-[*] Black Passed Evaluation: {MG: 0.00, EG: 0.00}
-[*] White Space Evaluation: {MG: 0.40, EG: 0.00}
-[*] Black Space Evaluation: {MG: 0.40, EG: 0.00}
-[*] Evaluation: 0.31 {white}
-*/
